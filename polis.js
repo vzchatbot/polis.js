9839bdfeeb6703346fe929ec43d29f130132097a
hramprasath <hramprasath@gmail.com>
2016-09-04 14:16:36 +0530

    Update polis.js

--- polis.js
+++ polis.js
@@ -1,15 +1,11 @@
 var express = require('express'); // call express
 var app = express(); // define our app using express
 var bodyParser = require('body-parser');
-// configure app to use bodyParser()
-// this will let us get the data from a POST
-console.log('my message server started...');
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-var port = process.env.PORT || 5000; // set our port
-var router = express.Router(); 
 
-router.post('/webhook', function (req, res) {
+var port = process.env.PORT || 5000; // set our port
+
+
+app.post('/webhook', function (req, res) {
 var intent = req.body.result.metadata.intentName;
 //var intent = 'Initiate';
     switch (intent) {
