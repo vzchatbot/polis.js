b577279672deee12cba2cdc1d7353b49dea26a2f
hramprasath <hramprasath@gmail.com>
2016-09-04 17:40:27 +0530

    Update servicecall.js

--- servicecall.js
+++ servicecall.js
@@ -47,3 +47,41 @@ function performRequest(endpoint, method, data, success) {
   req.write(dataString);
   req.end();
 }
+function testfunction (req,res)
+{
+  
+  return ({
+        speech: "From another file e. I am Ent, an entertainment bot.  Would you like to see some recommendations for tonight?",
+        displayText: "TV Recommendations",
+        data: {
+            "facebook": {
+                "attachment": {
+                    "type": "template",
+                    "payload": {
+                        "template_type": "button",
+                        "text": "Hi,there. I am Ent, an entertainment bot.  Would you like to see some recommendations for tonight?",
+                        //"template_type":"generic",
+                        //"elements":[
+                        //	{
+                        //		"title":"Hi,there. I am Ent, an entertainment bot.",
+                        //		"image_url":"https://petersfancybrownhats.com/company_image.png",
+                        //		"subtitle":"Would you like to see some recommendations for tonight?",
+                        "buttons": [
+                            {
+                                "type": "postback",
+                                "title": "Yes",
+                                "payload": "Yes"
+                            },
+                            {
+                                "type": "postback",
+                                "title": "No",
+                                "payload": "No"
+                            }
+                        ]
+                    }
+                }
+            }
+        },
+        source: "Zero Service - app_zero.js"
+    });
+}
