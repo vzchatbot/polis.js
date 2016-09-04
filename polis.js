5a2933a3648d4306ce0b10762565e73c13822923
hramprasath <hramprasath@gmail.com>
2016-09-04 13:18:39 +0530

    Update polis.js

--- polis.js
+++ polis.js
@@ -5,34 +5,6 @@ var PORT = process.env.PORT || 9000;
 
 var app = express();
 
-app.post('/webhook', function (req, res) {
-  return({
- "speech": "Toda1y in Boston: Fair, the temperature is 37 F",
-  "source": "apiai-weather-webhook-sample",
-  "displayText": "Today in Boston: Fair, the temperature is 37 F"});
-}
-  
-function payloadProcessor (payload, done) {
-  // Do stuff with payload like update records in a database,
-  // send text messages to field staff, email supervisors when
-  // task marked complete, etc.
-
-  // After you've processed the payload call done() with no arguments to signal
-  // that the webhook has been processed. Call done(), passing an error to return
-  // a 500 response to the webhook request, signaling that the request should be
-  // tried again later.
-  console.log('Payload:');
-  console.log(payload);
-  done()
-}
-
-var fulcrumMiddlewareConfig = {
-  actions: ['record.create', 'record.update'],
-  processor: payloadProcessor
-};
-
-app.use('/', fulcrumMiddleware(fulcrumMiddlewareConfig));
-
 app.post('/', function (req, res) {
   //res.send('<html><head><title>Polis.js</title></head><body><h2>polis.js</h2><p>Up and Running!</p></body></html>');
  // res.send('test message');
