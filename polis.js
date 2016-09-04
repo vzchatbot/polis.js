a868068d8e6fd21f648ae2f4db9a0be6121d01d6
hramprasath <hramprasath@gmail.com>
2016-09-04 14:15:03 +0530

    Update polis.js

--- polis.js
+++ polis.js
@@ -7,9 +7,9 @@ console.log('my message server started...');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 var port = process.env.PORT || 5000; // set our port
+var router = express.Router(); 
 
-
-app.post('/webhook', function (req, res) {
+router.post('/webhook', function (req, res) {
 var intent = req.body.result.metadata.intentName;
 //var intent = 'Initiate';
     switch (intent) {
