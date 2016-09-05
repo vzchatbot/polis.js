84d3fda81ea8397c03b5c2eda6e491c90f7bf1e3
hramprasath <hramprasath@gmail.com>
2016-09-05 16:55:14 +0530

    Update polis.js

--- polis.js
+++ polis.js
@@ -41,7 +41,7 @@ router.post('/webhook', function (req, res) {
 function performcall(){
 
 
-request('http://code.jsontest.com/', function (error, response, body) {
+request('http://date.jsontest.com/', function (error, response, body) {
     if (!error && response.statusCode == 200) {
         console.log(body) // Print the google web page.
         return{
