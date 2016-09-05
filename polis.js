6e2cfd29e58e0da7d5437b08cdd827798ab166ae
hramprasath <hramprasath@gmail.com>
2016-09-05 19:17:53 +0530

    Update polis.js

--- polis.js
+++ polis.js
@@ -51,47 +51,13 @@ request('http://ip.jsontest.com/', function (error, response, body) {
      }
 })
 
-}
+};
 
 
 
 
 
 
- return ({
-        speech: "Welcome Hi,there. I am Ent, an entertainment bot.  Would you like to see some recommendations for tonight?",
-        displayText: "TV Recommendations",
-        data: {
-            "facebook": {
-                "attachment": {
-                    "type": "template",
-                    "payload": {
-                        "template_type": "button",
-                        "text": "Hi,there. I am Ent, an entertainment bot.  Would you like to see some recommendations for tonight?",
-                        //"template_type":"generic",
-                        //"elements":[
-                        //	{
-                        //		"title":"Hi,there. I am Ent, an entertainment bot.",
-                        //		"image_url":"https://petersfancybrownhats.com/company_image.png",
-                        //		"subtitle":"Would you like to see some recommendations for tonight?",
-                        "buttons": [
-                            {
-                                "type": "postback",
-                                "title": "Yes",
-                                "payload": "Yes"
-                            },
-                            {
-                                "type": "postback",
-                                "title": "No",
-                                "payload": "No"
-                            }
-                        ]
-                    }
-                }
-            }
-        },
-        source: "Zero Service - app_zero.js"
-    });
 
 
 
