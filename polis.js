092fb6c9296fe5b97220441487a8d65a0533a8f8
hramprasath <hramprasath@gmail.com>
2016-09-06 13:36:09 +0530

    Update polis.js

--- polis.js
+++ polis.js
@@ -209,9 +209,80 @@ function recommendTV() {
         source: "Zero Service - app_zero.js"
     });
 }
+
+function pgmDetails() {
+    return ({
+        speech: "Here are some recommendations for tonight",
+        displayText: "TV recommendations",
+        data: {
+            "facebook": {
+                "attachment": {
+                    "type": "template",
+                    "payload": {
+                        "template_type": "generic",
+                        "elements": [
+                            {
+                                "title": "Shark Tank",
+                                "subtitle": "Shark Tank",
+                                "image_url": "http://image.vam.synacor.com.edgesuite.net/0f/07/0f07592094a2a596d2f6646271e9cb0311508415/w=414,h=303,crop=auto/?sig=88c390c980d4fa53d37ef16fbdc53ec3dfbad7d9fa626949827b76ae37140ac3&amp;app=powerplay",
+                                "buttons": [
+                                    {
+                                        "type": "web_url",
+                                        "url": "http://www.youtube.com/embed/SQ1W7RsXL3k",
+                                        "title": "Watch video"
+                                    },
+                                    {
+                                        "type": "web_url",
+                                        "url": "https://m.verizon.com/myverizonmobile/router.aspx?token=tvlisting",
+                                        "title": "Record"
+                                    }
+                                ]
+                            },
+                            {
+                                "title": "Game of Thrones",
+                                "subtitle": "Game of Thrones",
+                                "image_url": "http://ia.media-imdb.com/images/M/MV5BMjM5OTQ1MTY5Nl5BMl5BanBnXkFtZTgwMjM3NzMxODE@._V1_UX182_CR0,0,182,268_AL_.jpg",
+                                "buttons": [
+                                    {
+                                        "type": "web_url",
+                                        "url": "https://www.youtube.com/watch?v=36q5NnL3uSM",
+                                        "title": "Watch video"
+                                    },
+                                    {
+                                        "type": "web_url",
+                                        "url": "https://m.verizon.com/myverizonmobile/router.aspx?token=tvlisting",
+                                        "title": "Record"
+                                    }
+                                ]
+                            },
+                            {
+                                "title": "The Night Of",
+                                "subtitle": "The Night Of",
+                                "image_url": "http://ia.media-imdb.com/images/M/MV5BMjQyOTgxMDI0Nl5BMl5BanBnXkFtZTgwOTE4MzczOTE@._V1_UX182_CR0,0,182,268_AL_.jpg",
+                                "buttons": [
+                                    {
+                                        "type": "web_url",
+                                        "url": "https://www.youtube.com/watch?v=36q5NnL3uSM",
+                                        "title": "Watch video"
+                                    },
+                                    {
+                                        "type": "web_url",
+                                        "url": "https://m.verizon.com/myverizonmobile/router.aspx?token=tvlisting",
+                                        "title": "Record"
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                }
+            }
+        },
+        source: "Zero Service - app_zero.js"
+    });
+}
 function chatInitiate() {
     return ({
-        speech: "Welcome Hi,there. I am Ent, an entertainment bot.  Would you like to see some recommendations for tonight?",
+        speech: "Hi, I am Verizon Entertainment bot.  Would you like to see some TV Recommendations for tonight?",
         displayText: "TV Recommendations",
         data: {
             "facebook": {
