3b1ca0c0276738cc989fc75ec4a19a37bdb249c9
hramprasath <hramprasath@gmail.com>
2016-09-07 13:06:43 +0530

    Update polis.js

--- polis.js
+++ polis.js
@@ -31,6 +31,9 @@ router.post('/webhook', function (req, res) {
         case "record":
             res.json(record(req));
             break;
+        case "record":
+            res.json(upsell(req));
+            break;
         case "externalcall":
             res.json(performcall());
             break;
