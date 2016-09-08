dec2eda077e2699abe9fe3a5d02487352bc7d7e3
hramprasath <hramprasath@gmail.com>
2016-09-08 14:49:11 +0530

    Update polis.js

--- polis.js
+++ polis.js
@@ -64,7 +64,7 @@ function recommendTVNew(callback) {
      return ({ 
 	         speech: "Here are some recommendations for tonight", 
          displayText: "TV recommendations", 
-         data:  jsonresp.facebook, 
+         data:  jsonresp.facebook[0], 
          source: "Zero Service - app_zero.js" 
      }); 
  } 
