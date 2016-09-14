d4d3633adfef1bc5be055227cc211242adcd804a
hramprasath <hramprasath@gmail.com>
2016-09-14 19:50:26 +0530

    Update polis.js

--- polis.js
+++ polis.js
@@ -54,7 +54,6 @@ function recommendTVNew(callback) {
      //{"Flow": "TroubleShooting Flows\\Test\\APIChatBot.xml","Request":{"ThisValue":"Trending"}}
      request.post( 
          'http://www.verizon.com/msvsearch/whatshot/tv?op=now&region=91812&lrg=1', 
-         '{"Flow": "TroubleShooting Flows\\Test\\APIChatBot.xml","Request":{"ThisValue":"Trending"}}',
          function (error, response, body) { console.log('inside external call');
              if (!error && response.statusCode == 200) { 
              	console.log('inside external call success');
