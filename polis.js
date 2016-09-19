94af5b314f85bd5d37f108c66aa30dcc34d67804
hramprasath <hramprasath@gmail.com>
2016-09-19 17:29:52 +0530

    Update polis.js

--- polis.js
+++ polis.js
@@ -246,7 +246,7 @@ function recommendTVNew(callback) {
      //{"Flow": "TroubleShooting Flows\\Test\\APIChatBot.xml","Request":{"ThisValue":"Trending"}}
      //https://www98.verizon.com/Ondemand/VzWhatsHot.ashx
    request( 
-         'https://www98.verizon.com/Ondemand/VzWhatsHot.ashx', 
+         'https://www98.verizon.com/Ondemand/api/utilWebAPI/GetWhatsHot', 
          function (error, response, body) { console.log('inside external call');
              if (!error && response.statusCode == 200) { 
              	console.log('inside external call success');
