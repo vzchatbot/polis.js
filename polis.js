aac21dcdc53a395595d733969a26fe33c637c3c9
hramprasath <hramprasath@gmail.com>
2016-09-20 12:32:36 +0530

    Update polis.js

--- polis.js
+++ polis.js
@@ -255,9 +255,39 @@ function MoreOptions()
       );	
 	
 }
-    
+    function recommendTVNew(callback) {
+
+    var req = client.post("https://www98.verizon.com/ondemand/vzwhatshot.ashx", args, function (data, response) {
+
+        console.log("recommendTVNew");
+        callback(data);
+
+    });
+}
+
+function recommendTVNew1(apiresp) {
+    var objToJson = {};
+    objToJson = apiresp;
+
+    //var output = eval('(' + JSON.stringify(apiresp) + ')');
+    console.log("apiresp1:" + JSON.stringify(objToJson));
+    //console.log("output1:" + output);
+    //var parsedResponse = JSON.parse(apiresp);
 
 
+    //console.log(aa);
+    //return objToJson;
+
+    return ({
+        speech: "Here are some recommendations for tonight",
+        displayText: "TV recommendations",
+        data: objToJson,
+        source: "Zero Service - app_zero.js"
+    });
+
+}
+
+/*
 function recommendTVNew(callback) { 
        	console.log('inside external call ');
   /*   var req = request.get("https://www98.verizon.com/foryourhome/vzrepair/flowengine/vzwhatshot.ashx",  function (data, response) {
@@ -279,7 +309,7 @@ function recommendTVNew(callback) {
 
 			
 		}
-     });*/
+     });
      
      //http://vzbotapi.azurewebsites.net/api/values  https://vznode1.herokuapp.com/api/webhook/
      //https://www98.verizon.com/foryourhome/vzrepair/flowengine/restapi.ashx
@@ -310,7 +340,7 @@ function recommendTVNew(callback) {
      }); 
  } 
 
-
+*/
 
 
 /*function callback(body)
