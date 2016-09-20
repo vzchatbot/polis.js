fc2dde9e57ab2c214bfc43cc83fb2e88bb2d643b
hramprasath <hramprasath@gmail.com>
2016-09-20 14:34:52 +0530

    Update polis.js

--- polis.js
+++ polis.js
@@ -61,7 +61,8 @@ res.header("Access-Control-Allow-Headers", "X-Requested-With");
              res.json(MoreOptions());
             break;
         case "Billing":
-            res.json(billInquiry());
+           // res.json(billInquiry());
+           res.json(myextcall());
             break;
         case "showrecommendation":
             res.json(recommendTV());
@@ -88,6 +89,72 @@ res.header("Access-Control-Allow-Headers", "X-Requested-With");
             res.json(recommendTV());
     }
 });
+
+
+function myextcall()
+{
+console.log("Inside myextcall");
+var headersInfo = { "Content-Type": "application/json" };
+var lookupQuestion = function (responseText, prevRequest, fnCallback) {
+	var reqData = { "Flow": "TroubleShooting Flows\\Test\\APIChatBot.xml", "Request": { "ThisValue": "1" } };
+	if (null != prevRequest && null != prevRequest["Request"]) {
+		reqData = prevRequest;
+	}
+	else {
+		headersInfo = { "Content-Type": "application/json" };	
+	}
+	
+	var Client = require('node-rest-client').Client;
+	var client = new Client();
+	var args = {
+		"headers": headersInfo,
+		"data": JSON.stringify(reqData)
+	};
+	var req = client.post("https://www98.verizon.com/foryourhome/vzrepair/flowengine/restapi.ashx", args, function (data, response) {
+		try {			
+			var parsedData = "";
+			if (null != data) {
+				parsedData = JSON.parse(data);
+				var inputsJSON = parsedData[0];
+				headersInfo = response.headers;
+
+				if (null != fnCallback && typeof fnCallback == "function") {
+					console.log(inputsJSON);
+				}
+			}
+			else {
+				var err = {
+					"description" : "Response data is empty!",
+					"data" : data
+				};
+				if (null != fnCallback && typeof fnCallback == "function") {
+					console.log(err, null);
+				}
+			}
+		}
+        catch (ex) {
+			var err = {
+				"description" : "Exception occurred:" + ex,
+				"data" : data
+			};
+			if (null != fnCallback && typeof fnCallback == "function") {
+				console.log(err, null);
+			}
+		}
+	});
+	req.on("error", function (errInfo) {
+		var err = {
+			"description" : "Exception occurred:" + errInfo.message,
+			"data" : ""
+		};
+		if (null != fnCallback && typeof fnCallback == "function") {
+			console.log(err, null);
+		}
+	});
+};
+
+}
+
 
   function recommendTVNew(callback) {
 //https://www98.verizon.com/Ondemand/api/utilWebAPI/GetWhatsHot
