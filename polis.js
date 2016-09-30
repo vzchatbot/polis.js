df55d0dd5159fcd9b4b082c90e7fc975dc281b18
hramprasath <hramprasath@gmail.com>
2016-09-30 15:03:35 +0530

    Update polis.js

--- polis.js
+++ polis.js
@@ -95,8 +95,8 @@ var channel = apireq.body.result.parameters.Channel.toUpperCase() ;
 	   {
         speech: "You can watch " + channel + "  on Channel Number : "+ chnNo ,
         displayText: "You can watch " + channel + "  on Channel Number : "+ chnNo ,
-	"data": {"facebook": { "text":  " First Messge",}},
-	"data": {"facebook": { "text":  " Second Messge",}},		   
+	"data": {"facebook": { "text":  " First Messge", "text":  " Second Messge"}},
+	//"data": {"facebook": { "text":  " Second Messge",}},		   
         source: "Verizon.js"
     	}
 	 /*  {
