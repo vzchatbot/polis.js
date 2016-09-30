e4716366960f1867492d19a3db70084e5f9a6387
hramprasath <hramprasath@gmail.com>
2016-09-30 12:16:57 +0530

    Update polis.js

--- polis.js
+++ polis.js
@@ -97,11 +97,11 @@ var channel = apireq.body.result.parameters.Channel.toUpperCase() ;
         displayText: "You can watch " + channel + "  on Channel Number : "+ chnNo ,
         source: "Verizon.js"
     	}
-	   {
+	 /*  {
         speech: "Second Message You can watch " + channel + "  on Channel Number : "+ chnNo ,
         displayText: "Second Message You can watch " + channel + "  on Channel Number : "+ chnNo ,
         source: "Verizon.js"
-    }
+    }*/
 	  
 	  
 	  
