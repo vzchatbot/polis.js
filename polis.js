746a4728e0f5ebd737affc03af04dac84bf1e083
hramprasath <hramprasath@gmail.com>
2016-10-03 13:21:26 +0530

    Update polis.js

--- polis.js
+++ polis.js
@@ -102,11 +102,14 @@ function firstMsg() {
 } 
 
 function secondMsg() {
-   return (
+  
+	var stblst= {"facebook":{"attachment":{"type":"template","payload":{"template_type":"button","text":"Select one of the STB from the below list, on which you like to record","buttons":[{"type":"postback","payload":"0000075999169227","title":"0000075999169227"},{"dummy":"dummy"}]}}}};
+//{ "facebook": { "attachment": { "type": "template", "payload": { "template_type": "button", "text": "Select one of the STB from the below list, on which you like to record", "buttons":[ { "type": "postback", "payload": "0000060661164198", "title": "Living Room" } ]} } } }	
+	return (
 	 {
         speech: "Second Message",
         displayText: "Second Message",
-	data: { "facebook": { "attachment": { "type": "template", "payload": { "template_type": "button", "text": "Select one of the STB from the below list, on which you like to record", "buttons":[ { "type": "postback", "payload": "0000060661164198", "title": "Living Room" } ]} } } },
+	data:stblst ,
         source: "Verizon.js"
     	}  );
 } 
