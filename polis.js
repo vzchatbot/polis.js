8c7012a16e1efabaad743a12a8df4b8758c748a9
hramprasath <hramprasath@gmail.com>
2016-10-04 15:55:31 +0530

    Update polis.js

--- polis.js
+++ polis.js
@@ -216,7 +216,7 @@ function recommendTVNew1(apiresp) {
 
 function STBList(apireq,callback) { 
        	console.log('inside external call '+ apireq.body.contexts);
-	var struserid = apireq.body.result.contexts.parameters.Userid; 
+	var struserid = apireq.body.result.contexts[0].parameters.Userid; 
 		console.log('struserid '+ struserid);
         var headersInfo = { "Content-Type": "application/json" };
 	var args = {
