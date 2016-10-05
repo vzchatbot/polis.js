6d305323872ae1fde7562a9511a838adaa85b868
hramprasath <hramprasath@gmail.com>
2016-10-05 19:10:28 +0530

    Update polis.js

--- polis.js
+++ polis.js
@@ -323,56 +323,6 @@ function PgmSearchCallback(apiresp) {
 
 } 
 
-
-function recommendTVStg(callback) { 
-       	console.log('inside external call ');
-        var headersInfo = { "Content-Type": "application/json" };
-	var args = {
-		"headers": headersInfo,
-		"json": {
-			Flow: 'TroubleShooting Flows\\Test\\APIChatBot.xml',
-			Request: {
-				ThisValue: 'Trending1'
-			}
-		}
-	};
-
-    request.post("https://www98.verizon.com/foryourhome/vzrepair/flowengine/restapi.ashx", args,
-        function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-             
-                 console.log("body " + body);
-                callback(body);
-            }
-            else
-            	console.log('error: ' + error + ' body: ' + body);
-        }
-    );
- } 
-
-function welcomeInit()
-{
-  var username="";
-  return (
-      { speech: " Hey "+ username +"Welcome to Verizon!",
-          displayText: " Hey Tabi, Welcome to Verizon!",
-        data: {
-		"facebook": [
-			{"text": "Here is a video to watch:"},
-			{"sender_action": "typing_on"},
-			{
-			"attachment": {
-			"type": "video",
-			"payload": {"url": "http://path.to/video.mp4"}
-				      }
-			}
-		]
-	   }
-      
-        }
-     );	
-}
-
 function upgradeDVR(apireq)
 {
    var confirm =  apireq.body.result.parameters.Confirm.toUpperCase();
@@ -516,7 +466,6 @@ function getstblist(apireq,callback)
 			STBListCallBackNew(str,callback);
 			});
 }
-
 
 function record(apireq)
 {
@@ -738,196 +687,6 @@ var respstr ='Congrats, Now you are subscribed for ' + apireq.body.result.parame
     });
 }
 
-function performcall1(req,res){
-	
-	res.end('welcome');
-}
-
-
-function performcall(req,res){
-console.log("starting performcall");
-
-console.log(performcall1());
-
-	var myresp='';
-	return request.post({
-	  headers: {'content-type' : 'application/x-www-form-urlencoded'},
-	  url:     'https://vznode1.herokuapp.com/api/webhook/',
-	  body:    "mes=heydude"
-	}, function(error, response, body)
-		{
-			console.log("inside fn call");
-			if (!error && response.statusCode == 200) 
-    			{
-    			//console.log(body); // Print the google web page.
-    			//callback(body);
-			myresp = body;
-			
-			console.log(myresp);
-    			}
-    			 else
-			     {
-			     	console.log(error);
-			     	console.log(response.statusCode);
-			     	myresp=recommendTV();
-			     	
-			     }
-		}
-	);
-
-};
-
-function callapi(callback){
-//http://vzbotapi.azurewebsites.net/api/values
-	request.post( 
-        'https://vznode1.herokuapp.com/api/webhook/', 
-         function (error, response, body) { 
-            if (!error && response.statusCode == 200) { 
-                callback(body); 
-            } 
-        } 
-    );
-	
-	
-};
-
-function performRequest(endpoint, method, data, success) {
-  console.log('staring performRequest ');
-  var querystring = require('querystring');
-var https = require('https');
-
-var host = '';
-var username = '';
-var password = '';
-var apiKey = '';
-var sessionId = null;
-var deckId = '68DC5A20-EE4F-11E2-A00C-0858C0D5C2ED';
-var responseObject='';
- var responseString = '';
- 
-  var dataString = JSON.stringify(data);
-  var headers = {};
-  
-  if (method == 'GET') {
-    endpoint += '?' + querystring.stringify(data);
-  }
-  else {
-    headers = {
-      'Content-Type': 'application/json',
-      'Content-Length': dataString.length
-    };
-  }
-  var options = {
-    host: host,
-    path: endpoint,
-    method: method,
-    headers: headers
-  };
-
-  var req = https.request(options, function(res) {
-    res.setEncoding('utf-8');
-
-   
-
-    res.on('data', function(data) {
-      responseString += data;
-   
-    });
-
-    res.on('end', function() {
-      console.log('responseString:'+responseString);
-       responseObject = JSON.parse(responseString);
-       //console.log('responseObject:'+ responseObject);
-       //console.log('dataString:'+ dataString);
-       
-      success(responseObject);
-    });
-  });
-//console.log('endpoint:'+ endpoint);
-  req.write(dataString);
-  req.end();
-
-  return{
-    
-     speech:'response from call' ,
-        displayText: "TV recommendations",
-        data: responseObject,
-         source: "test functions"
-  }
- };
-
-
-function recommendTV() {
-    return ({
-        speech: "Here are some recommendations for tonight",
-        displayText: "TV recommendations",
-        data: {
-            "facebook": {
-                "attachment": {
-                    "type": "template",
-                    "payload": {
-                        "template_type": "generic",
-                        "elements": [
-                            {
-                                "title": "Shark Tank",
-                                "subtitle": "Shark Tank",
-                                "image_url": "http://image.vam.synacor.com.edgesuite.net/0f/07/0f07592094a2a596d2f6646271e9cb0311508415/w=414,h=303,crop=auto/?sig=88c390c980d4fa53d37ef16fbdc53ec3dfbad7d9fa626949827b76ae37140ac3&amp;app=powerplay",
-                                "buttons": [
-                                    {
-                                        "type": "web_url",
-                                        "url": "http://www.youtube.com/embed/SQ1W7RsXL3k",
-                                        "title": "Watch video"
-                                    },
-                                    {
-                                        "type": "web_url",
-                                        "url": "https://m.verizon.com/myverizonmobile/router.aspx?token=tvlisting",
-                                        "title": "Record"
-                                    }
-                                ]
-                            },
-                            {
-                                "title": "Game of Thrones",
-                                "subtitle": "Game of Thrones",
-                                "image_url": "http://ia.media-imdb.com/images/M/MV5BMjM5OTQ1MTY5Nl5BMl5BanBnXkFtZTgwMjM3NzMxODE@._V1_UX182_CR0,0,182,268_AL_.jpg",
-                                "buttons": [
-                                    {
-                                        "type": "web_url",
-                                        "url": "https://www.youtube.com/watch?v=36q5NnL3uSM",
-                                        "title": "Watch video"
-                                    },
-                                    {
-                                        "type": "web_url",
-                                        "url": "https://m.verizon.com/myverizonmobile/router.aspx?token=tvlisting",
-                                        "title": "Record"
-                                    }
-                                ]
-                            },
-                            {
-                                "title": "The Night Of",
-                                "subtitle": "The Night Of",
-                                "image_url": "http://ia.media-imdb.com/images/M/MV5BMjQyOTgxMDI0Nl5BMl5BanBnXkFtZTgwOTE4MzczOTE@._V1_UX182_CR0,0,182,268_AL_.jpg",
-                                "buttons": [
-                                    {
-                                        "type": "web_url",
-                                        "url": "https://www.youtube.com/watch?v=36q5NnL3uSM",
-                                        "title": "Watch video"
-                                    },
-                                    {
-                                        "type": "web_url",
-                                        "url": "https://m.verizon.com/myverizonmobile/router.aspx?token=tvlisting",
-                                        "title": "Record"
-                                    }
-                                ]
-                            }
-                        ]
-                    }
-                }
-            }
-        },
-        source: "Zero Service - app_zero.js"
-    });
-}
-
 function pgmDetails() {
     return ({
         speech: "Here are some recommendations for tonight",
@@ -1086,7 +845,7 @@ return ({
 
 } 
 
-
+//------------------dummy methods
 function myfunction() 
  {
 	console.log("inside fn call");
@@ -1138,6 +897,246 @@ function myfunction()
 		}
 	});
 };
+
+
+function performcall1(req,res){
+	
+	res.end('welcome');
+}
+
+
+function performcall(req,res){
+console.log("starting performcall");
+
+console.log(performcall1());
+
+	var myresp='';
+	return request.post({
+	  headers: {'content-type' : 'application/x-www-form-urlencoded'},
+	  url:     'https://vznode1.herokuapp.com/api/webhook/',
+	  body:    "mes=heydude"
+	}, function(error, response, body)
+		{
+			console.log("inside fn call");
+			if (!error && response.statusCode == 200) 
+    			{
+    			//console.log(body); // Print the google web page.
+    			//callback(body);
+			myresp = body;
+			
+			console.log(myresp);
+    			}
+    			 else
+			     {
+			     	console.log(error);
+			     	console.log(response.statusCode);
+			     	myresp=recommendTV();
+			     	
+			     }
+		}
+	);
+
+};
+
+
+function callapi(callback){
+//http://vzbotapi.azurewebsites.net/api/values
+	request.post( 
+        'https://vznode1.herokuapp.com/api/webhook/', 
+         function (error, response, body) { 
+            if (!error && response.statusCode == 200) { 
+                callback(body); 
+            } 
+        } 
+    );
+	
+	
+};
+
+function performRequest(endpoint, method, data, success) {
+  console.log('staring performRequest ');
+  var querystring = require('querystring');
+var https = require('https');
+
+var host = '';
+var username = '';
+var password = '';
+var apiKey = '';
+var sessionId = null;
+var deckId = '68DC5A20-EE4F-11E2-A00C-0858C0D5C2ED';
+var responseObject='';
+ var responseString = '';
+ 
+  var dataString = JSON.stringify(data);
+  var headers = {};
+  
+  if (method == 'GET') {
+    endpoint += '?' + querystring.stringify(data);
+  }
+  else {
+    headers = {
+      'Content-Type': 'application/json',
+      'Content-Length': dataString.length
+    };
+  }
+  var options = {
+    host: host,
+    path: endpoint,
+    method: method,
+    headers: headers
+  };
+
+  var req = https.request(options, function(res) {
+    res.setEncoding('utf-8');
+
+   
+
+    res.on('data', function(data) {
+      responseString += data;
+   
+    });
+
+    res.on('end', function() {
+      console.log('responseString:'+responseString);
+       responseObject = JSON.parse(responseString);
+       //console.log('responseObject:'+ responseObject);
+       //console.log('dataString:'+ dataString);
+       
+      success(responseObject);
+    });
+  });
+//console.log('endpoint:'+ endpoint);
+  req.write(dataString);
+  req.end();
+
+  return{
+    
+     speech:'response from call' ,
+        displayText: "TV recommendations",
+        data: responseObject,
+         source: "test functions"
+  }
+ };
+
+function recommendTVStg(callback) { 
+       	console.log('inside external call ');
+        var headersInfo = { "Content-Type": "application/json" };
+	var args = {
+		"headers": headersInfo,
+		"json": {
+			Flow: 'TroubleShooting Flows\\Test\\APIChatBot.xml',
+			Request: {
+				ThisValue: 'Trending1'
+			}
+		}
+	};
+
+    request.post("https://www98.verizon.com/foryourhome/vzrepair/flowengine/restapi.ashx", args,
+        function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+             
+                 console.log("body " + body);
+                callback(body);
+            }
+            else
+            	console.log('error: ' + error + ' body: ' + body);
+        }
+    );
+ } 
+
+function welcomeInit()
+{
+  var username="";
+  return (
+      { speech: " Hey "+ username +"Welcome to Verizon!",
+          displayText: " Hey Tabi, Welcome to Verizon!",
+        data: {
+		"facebook": [
+			{"text": "Here is a video to watch:"},
+			{"sender_action": "typing_on"},
+			{
+			"attachment": {
+			"type": "video",
+			"payload": {"url": "http://path.to/video.mp4"}
+				      }
+			}
+		]
+	   }
+      
+        }
+     );	
+}
+
+function recommendTV() {
+    return ({
+        speech: "Here are some recommendations for tonight",
+        displayText: "TV recommendations",
+        data: {
+            "facebook": {
+                "attachment": {
+                    "type": "template",
+                    "payload": {
+                        "template_type": "generic",
+                        "elements": [
+                            {
+                                "title": "Shark Tank",
+                                "subtitle": "Shark Tank",
+                                "image_url": "http://image.vam.synacor.com.edgesuite.net/0f/07/0f07592094a2a596d2f6646271e9cb0311508415/w=414,h=303,crop=auto/?sig=88c390c980d4fa53d37ef16fbdc53ec3dfbad7d9fa626949827b76ae37140ac3&amp;app=powerplay",
+                                "buttons": [
+                                    {
+                                        "type": "web_url",
+                                        "url": "http://www.youtube.com/embed/SQ1W7RsXL3k",
+                                        "title": "Watch video"
+                                    },
+                                    {
+                                        "type": "web_url",
+                                        "url": "https://m.verizon.com/myverizonmobile/router.aspx?token=tvlisting",
+                                        "title": "Record"
+                                    }
+                                ]
+                            },
+                            {
+                                "title": "Game of Thrones",
+                                "subtitle": "Game of Thrones",
+                                "image_url": "http://ia.media-imdb.com/images/M/MV5BMjM5OTQ1MTY5Nl5BMl5BanBnXkFtZTgwMjM3NzMxODE@._V1_UX182_CR0,0,182,268_AL_.jpg",
+                                "buttons": [
+                                    {
+                                        "type": "web_url",
+                                        "url": "https://www.youtube.com/watch?v=36q5NnL3uSM",
+                                        "title": "Watch video"
+                                    },
+                                    {
+                                        "type": "web_url",
+                                        "url": "https://m.verizon.com/myverizonmobile/router.aspx?token=tvlisting",
+                                        "title": "Record"
+                                    }
+                                ]
+                            },
+                            {
+                                "title": "The Night Of",
+                                "subtitle": "The Night Of",
+                                "image_url": "http://ia.media-imdb.com/images/M/MV5BMjQyOTgxMDI0Nl5BMl5BanBnXkFtZTgwOTE4MzczOTE@._V1_UX182_CR0,0,182,268_AL_.jpg",
+                                "buttons": [
+                                    {
+                                        "type": "web_url",
+                                        "url": "https://www.youtube.com/watch?v=36q5NnL3uSM",
+                                        "title": "Watch video"
+                                    },
+                                    {
+                                        "type": "web_url",
+                                        "url": "https://m.verizon.com/myverizonmobile/router.aspx?token=tvlisting",
+                                        "title": "Record"
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                }
+            }
+        },
+        source: "Zero Service - app_zero.js"
+    });
+}
 
 function billInquiry() {
     return ({
