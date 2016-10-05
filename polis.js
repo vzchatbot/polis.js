e206d526e69ceab27d14445b1f8d08d47a44bd78
hramprasath <hramprasath@gmail.com>
2016-10-05 18:31:28 +0530

    Update polis.js

--- polis.js
+++ polis.js
@@ -52,7 +52,7 @@ res.header("Access-Control-Allow-Headers", "X-Requested-With");
            res.json(record(req));
             break;
 	 case "stblist":
-           recordnew(req,function (subflow){res.json(subflow);});
+           getstblist(req,function (subflow){res.json(subflow);});
             break;
         case "upsell":
             res.json(upsell(req));
@@ -508,163 +508,13 @@ function MoreOptions()
       );	
 	
 }
-function recordnew(apireq,callback)
-{
-	
-var channel = apireq.body.result.parameters.Channel.toUpperCase() ;
-var program = apireq.body.result.parameters.Programs.toUpperCase();
-var time = apireq.body.result.parameters.timeofpgm;
-var dateofrecord = apireq.body.result.parameters.date;
-var SelectedSTB = apireq.body.result.parameters.SelectedSTB;
-	console.log("SelectedSTB  :  "+  SelectedSTB + " channel : " + channel + " dateofrecord :" + dateofrecord +" time :" + time);
-if (time == "")
-{
-return (
-	
-	
-	{
-        speech: " I see the below schedules for " + program +". Tap on which time you like to record",
-        displayText: "Subscribe",
-        data: {
-            "facebook": {
-                "attachment": {
-                    "type": "template",
-                    "payload": {
-                        "template_type": "button",
-                        "text":  " I see the below schedules for " + program +". Tap on which time you like to record",
-                        "buttons": [
-                            {
-                                "type": "postback",
-                                "title": "12:30",
-                                "payload": "12:30"
-                            },
-                            {
-                                "type": "postback",
-                                "title": "10:30",
-                                "payload": "12:30"
-                            }
-                        ]
-                    }
-                }
-            }
-        },
-        source: "Zero Service - app_zero.js"
-    });		
-	
-}
 
-else if (channel == 'HBO')
+function getstblist(apireq,callback)
 {
-return ({
-        speech: " Sorry you are not subscribed to " + channel +". Would you like to subscribe " + channel + "?",
-        displayText: "Subscribe",
-        data: {
-            "facebook": {
-                "attachment": {
-                    "type": "template",
-                    "payload": {
-                        "template_type": "button",
-                        "text": " Sorry you are not subscribed to " + channel +". Would you like to subscribe " + channel + "?",
-                        "buttons": [
-                            {
-                                "type": "postback",
-                                "title": "Subscribe",
-                                "payload": "Subscribe"
-                            },
-                            {
-                                "type": "postback",
-                                "title": "No, I'll do it later ",
-                                "payload": "No Subscribe"
-                            }
-                        ]
-                    }
-                }
-            }
-        },
-        source: "Zero Service - app_zero.js"
-    });	
-	
-}
-else if (SelectedSTB == "" || SelectedSTB == undefined)
-{
-		STBList(apireq,
-			function (str) {
-				STBListCallBackNew(str,callback);
+	STBList(apireq,
+		function (str) {
+			STBListCallBackNew(str,callback);
 			});
-
-	
-	
-}	
-else if (channel == 'CBS')
-{
-return ({
-        speech: " Sorry your DVR storage is full.  Would you like to upgrade your DVR ?",
-        displayText: "Subscribe",
-        data: {
-            "facebook": {
-                "attachment": {
-                    "type": "template",
-                    "payload": {
-                        "template_type": "button",
-                        "text": " Sorry your DVR storage is full.  Would you like to upgrade your DVR ?",
-                        "buttons": [
-                            {
-                                "type": "postback",
-                                "title": "Upgrade my DVR",
-                                "payload": "Upgrade my DVR"
-                            },
-                            {
-                                "type": "postback",
-                                "title": "No, I'll do it later ",
-                                "payload": "No Upgrade"
-                            }
-                        ]
-                    }
-                }
-            }
-        },
-        source: "Zero Service - app_zero.js"
-    });	
-	
-}
-else	
-{	
-var respstr ='Your recording for "' + apireq.body.result.parameters.Programs +'" has been scheduled at '+ apireq.body.result.parameters.timeofpgm + ' on ' + apireq.body.result.parameters.SelectedSTB + ' STB.';
- return ({
-        speech: respstr + "  Would you like to see some other TV Recommendations for tonight?",
-        displayText: "TV Recommendations",
-        data: {
-            "facebook": {
-                "attachment": {
-                    "type": "template",
-                    "payload": {
-                        "template_type": "button",
-                        "text": respstr + "  Would you like to see some other TV Recommendations for tonight?",
-                        //"template_type":"generic",
-                        //"elements":[
-                        //	{
-                        //		"title":"Hi,there. I am Ent, an entertainment bot.",
-                        //		"image_url":"https://petersfancybrownhats.com/company_image.png",
-                        //		"subtitle":"Would you like to see some recommendations for tonight?",
-                        "buttons": [
-                            {
-                                "type": "postback",
-                                "title": "Show Recommendations",
-                                "payload": "Show Recommendations"
-                            },
-                            {
-                                "type": "postback",
-                                "title": "More Options",
-                                "payload": "More Options"
-                            }
-                        ]
-                    }
-                }
-            }
-        },
-        source: "Zero Service - app_zero.js"
-    });
-}
 }
 
 
