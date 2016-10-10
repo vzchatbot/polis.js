d33481abe7fbc13907f00dc6ee97b89f49b6f8d6
hramprasath <hramprasath@gmail.com>
2016-10-10 23:24:43 +0530

    Update polis.js

--- polis.js
+++ polis.js
@@ -204,8 +204,10 @@ function secondMsg(apireq) {
 	//var stblst={"facebook":{"attachment":{"type":"template","payload":{"template_type":"button","text":"Here is the program details you are looking for","buttons":[{"type":"postback","title":"84 - Bundesliga Highlights Show II -  Fox Sport 2 - Oct  4 2016 12:00AM - Sports &amp; Fitness","payload":"1"},{"type":"postback","title":"84 - World Poker Tour -  Fox Sport 2 - Oct  4 2016  1:00AM - SHOWS","payload":"1"},{"type":"postback","title":"84 - World Poker Tour -  Fox Sport 2 - Oct  4 2016  2:00AM - SHOWS","payload":"1"}]}}}};
 	 //  stblst=   STBList(apireq,function (str) {STBListCallBack(str);  })
 
-var stblst={"facebook":{ "text":"Pick a color:", "quick_replies":[ { "content_type":"text", "title":"Red", "payload":"red" }, { "content_type":"text", "title":"Green", "payload":"green" } ] }};
+//var stblst={"facebook":{ "text":"Pick a color:", "quick_replies":[ { "content_type":"text", "title":"Red", "payload":"red" }, { "content_type":"text", "title":"Green", "payload":"green" } ] }};
  
+var stblst={"facebook":{ "text":"Pick a color:", "quick_replies":[ { "content_type":"text", "title":"Red", "payload":"red", "image_url":"http://petersfantastichats.com/img/red.png" }, { "content_type":"text", "title":"Green", "payload":"green", "image_url":"http://petersfantastichats.com/img/green.png" }, { "content_type":"text", "title":"Blue", "payload":"blue", "image_url":"http://petersfantastichats.com/img/green.png" }, { "content_type":"text", "title":"yellow", "payload":"yellow", "image_url":"http://petersfantastichats.com/img/green.png" }, { "content_type":"text", "title":"i m trying how big the text can go", "payload":"green", "image_url":"http://petersfantastichats.com/img/green.png" }] }};
+
 	return (
 	 {
         speech: "Second Message",
